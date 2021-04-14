73b308b7fcfaa00698c2f8471aa159b4e184c104
RoyLei <leiyang@laka-inc.com>
2021-04-14 18:24:48 +0800

    更新内容支持展开。

--- web/js/index.js
+++ web/js/index.js
@@ -22,11 +22,50 @@ var main = new Vue({
 	            this.apps = this.apps.concat(response.data)
 	            this.apps_page++
 	            this.show_load_more_apps_button = response.data.length > 0
+
+	            this.apps.forEach((item, index) => {
+	            	item.isExpand = false
+	            	item.needShowExpand = false
+	            })
+
+	            // 展开收起
+				this.$nextTick(() => {
+					/* 获取文本所在的div */
+					if (this.apps.length === 0) {
+						return
+					}
+
+					let changelogDoms = this.$refs.changelog
+					console.log('changelogDoms = ' + changelogDoms)
+
+					changelogDoms.forEach((item, index) => {
+	            		let app = this.apps[index]
+	            		console.log(item)
+						let lineHeight = 22
+						let height = item.offsetHeight;
+						console.log('height = ' + height)
+						if (height > lineHeight * 2) {
+							app.isExpand = false
+							app.needShowExpand = true
+						} else {
+							app.needShowExpand = false
+						}
+						let indexOfApp = this.apps.indexOf(app)
+						Vue.set(this.apps, indexOfApp, app)
+						console.log('app.needShowExpand = ' + app.needShowExpand)
+	            	})
+				})
 	        });
 		},
 		viewAllVersion: function (e) {
 			window.location.href += "versionlist.html?platform=" + this.selected_paltform + "&bundleID=" + e.currentTarget.getAttribute('bundle-id')
-		}
+		},
+		expandClick: function(app) {
+			app.isExpand = !app.isExpand
+			let indexOfApp = this.apps.indexOf(app)
+			Vue.set(this.apps, indexOfApp, app)
+			console.log('app.guid = ' + app.guid)
+		},
 	}
 });
 new Vue({
--- web/js/versionlist.js
+++ web/js/versionlist.js
@@ -25,13 +25,58 @@ var main = new Vue({
 	            this.apps = this.apps.concat(response.data)
 	            this.page++
 	            this.show_load_more_apps_button = response.data.length > 0
+
+	            this.apps.forEach((item, index) => {
+	            	item.isExpand = false
+	            	item.needShowExpand = false
+	            })
+
+				console.log('apps = ' + this.apps)
+
+	            // 展开收起
+				this.$nextTick(() => {
+					/* 获取文本所在的div */
+					let changelogDoms = this.$refs.changelog
+					console.log('changelogDoms = ' + changelogDoms)
+
+					changelogDoms.forEach((item, index) => {
+	            		let app = this.apps[index]
+	            		console.log(item)
+						let lineHeight = 22
+						let height = item.offsetHeight;
+						console.log('height = ' + height)
+						if (height > lineHeight * 2) {
+							app.isExpand = false
+							app.needShowExpand = true
+						} else {
+							app.needShowExpand = false
+						}
+						let indexOfApp = this.apps.indexOf(app)
+						Vue.set(this.apps, indexOfApp, app)
+						console.log('app.needShowExpand = ' + app.needShowExpand)
+	            	})
+				})
 	        });
+		},
+		expandClick: function(app) {
+			app.isExpand = !app.isExpand
+			let indexOfApp = this.apps.indexOf(app)
+			Vue.set(this.apps, indexOfApp, app)
+			console.log('app.guid = ' + app.guid)
 		},
 	},
 	computed: {
 		has_data: function () {
 			return this.apps.length > 0
-		}
-	}
+		},
+		// isExpand: function (app) {
+		// 	return app.isExpand
+		// },
+		// needShowExpand: function (app) {
+		// 	return app.needShowExpand
+		// },
+	},
 });
+
 main.loadApps()
+
